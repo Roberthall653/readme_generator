e77132d9a93aa76dffb2be8694840b25d128aeaa
Roberthall653 <roberthall653@gmail.com>
2023-06-20 11:02:21 -0700

    initial setup and readme questions added.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['What is your Github Username?', 'What is your email address?', "What is your project's name?", 'Please write a short description of your project.', 'What sort of license should your project have?', 'What command should be run to install dependencies?', 'What command should be run to run tests?', 'What does the user need to know about using the repo?', 'What does the user need to know about contributing to the repo?'];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}

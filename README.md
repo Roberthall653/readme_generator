d84c522a3028c8b4dc5d13c3d61598f3263ba5b9
Roberthall653 <roberthall653@gmail.com>
2023-06-20 14:24:34 -0700

    finalized input display and added badges.

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# readme_generator
+# README generator
+<img src="https://img.shields.io/badge/License-MIT-blue">  
+
+## Description
+An application to make README files.
+
+## Table of Contents
+-[Installation](#installation)  
+-[Usage](#usage)  
+-[License](#license)  
+-[How to contribute](#contribute)  
+-[Tests](#tests)  
+-[Questions](#questions)  
+
+## Installation
+ To install necessary dependencies, run the following command: 
+ >npm i
+## Usage
+Please install dependencies before running program.
+## Credits
+## License
+This project is licensed under the MIT license
+## Contribute
+Please open a pull request!
+## Tests
+npm test
+## Questions
+If you have any questions about the repo, open an issue or contact me directly at roberthall653@gmail.com. You can find more of my work at [Roberthall653](https://github.com/Roberthall653)
--- index.js
+++ index.js
@@ -1,14 +1,99 @@
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-// TODO: Create an array of questions for user input
+
 const questions = ['What is your Github Username?', 'What is your email address?', "What is your project's name?", 'Please write a short description of your project.', 'What sort of license should your project have?', 'What command should be run to install dependencies?', 'What command should be run to run tests?', 'What does the user need to know about using the repo?', 'What does the user need to know about contributing to the repo?'];
+const [username, email, title, description, license, install, tests, info, contribute] = questions;
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const generateReadme = ({ username, email, title, description, license, install, tests, info, contribute }) =>
+    `# ${title}
+<img src="https://img.shields.io/badge/License-${license}-blue">  
 
-// TODO: Create a function to initialize app
-function init() {}
+## Description
+${description}
 
-// Function call to initialize app
-init();
+## Table of Contents
+-[Installation](#installation)  
+-[Usage](#usage)  
+-[License](#license)  
+-[How to contribute](#contribute)  
+-[Tests](#tests)  
+-[Questions](#questions)  
+
+## Installation
+ To install necessary dependencies, run the following command: 
+ >${install}
+## Usage
+${info}
+## Credits
+## License
+This project is licensed under the ${license} license
+## Contribute
+${contribute}
+## Tests
+${tests}
+## Questions
+If you have any questions about the repo, open an issue or contact me directly at ${email}. You can find more of my work at [${username}](https://github.com/${username})
+`
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'username',
+            message: username,
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: email,
+        },
+        {
+            type: 'input',
+            name: 'title',
+            message: title,
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: description,
+        },
+        {
+            type: 'list',
+            choices: ['MIT', 'APACHE2.0', 'GPL 3.0', 'BSD 3', 'none'],
+            name: 'license',
+            message: license,
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: install,
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: tests,
+        },
+        {
+            type: 'input',
+            name: 'info',
+            message: info,
+        },
+        {
+            type: 'input',
+            name: 'contribute',
+            message: contribute,
+        },
+    ])
+    .then((answers) => {
+        const readmePageContent = generateReadme(answers);
+
+        fs.writeFile('README.md', readmePageContent, (err) =>
+            err ? console.log(err) : console.log('Generating README...')
+        );
+    });
+
+// // TODO: Create a function to initialize app
+// function init() { }
+
+// // Function call to initialize app
+// init();
